4122499d6e674960562478c4edbf0e6d05ac68f3
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2017-06-13 00:20:39 +0530

    Create binaryConverter.py

--- binaryConverter.py
+++ binaryConverter.py
@@ -1,4 +1,4 @@
-class stack:
+class stack: # create stack class
     def __init__(self):
         self.items=[]
 
@@ -18,25 +18,25 @@ class stack:
     def size(self):
         return len(self.items)
 
-s=stack()
-s1=stack()
-n=int(raw_input("enter num\n"))
+s=stack() #s is a object of stack
+s1=stack() # object of stack
+n=int(raw_input("enter num\n")) # take the number what want to convert
 while n>0:
-    if n%2==0:
-        s.push(0)
+    if n%2==0: #check weather  n modul by 2
+        s.push(0) 
     else:
         s.push(1)
     n=n//2
 
 
-while not s.isEmpty():
-      s1.push(s.pop())
+while not s.isEmpty(): #take out the s stack
+      s1.push(s.pop()) # fill the s1 pasing values of s
 
     
-print(str(s1.items))
+print(str(s1.items)) #items of s1  display as qithin a []
 print ("Binary value ")
 for x in s1.items:
-    print(x),
+    print(x), #items of s1 
     
         
         
