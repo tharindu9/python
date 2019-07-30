d82be43c25905add472e66c0d7e8b02a935e1ebd
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2019-07-30 10:46:32 +0530

    Update and rename algo-in-python to algo-in-python.py

---
+++ algo-in-python.py
@@ -0,0 +1,18 @@
+//fibonachi
+
+
+n0 = 0;
+n1 =1;
+ans =0;
+for i in range(7):
+    if(i==0):
+        print(n0 , end=" ");
+
+    elif(i==1):
+        print(n1 , end=" ");
+
+    else:
+        ans = n0+n1;
+        n0=n1;
+        n1=ans;
+        print(ans , end=" ")
