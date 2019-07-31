49fe8dd856b3c23550e53900923788840299d73b
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2019-07-31 22:51:48 +0530

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,40 +1,37 @@
-import sys
-
 
 for i in range(1,9):
-    print("#", end=" ")
+    print("@", end=" ")
     if(i==3):
-        print("#","#")
+        print("@","@")
         for j in range(1,5):
-            print(" " , " ","#")
+            print(" " , " ","@")
 
-print('\n')
+print()
+
+for x in range(1,6):
+
+    if(x==1):
+        print()
+        print("  *","   *")
+    if(x==2):
+        print("*   *","   *")
+    if(x==3):
+        print("*","  "*3 , "*")
+    if(x==4):
+        print("  *","  ","*  ")
+    if(x==5):
+        print("   ","*" ,"  ")
+
 for k in range(1,6):
     if(k==1):
         for l in range(1,4):
             if(i!=3):
-                print("#")
+                print("#"," "*3,"#")
             else:
                 print("#",end="")
 
 
     else:
         print("#" , end=" ")
-
-print("\n")
-
-for k in range(1, 6):
-    if (k == 1):
-        for l in range(1, 4):
-            if (i != 3):
-                print("#")
-            else:
-                print("#", end="")
-
-
-    else:
-        print("#", end=" ")
-
-#sys.stdout.write('\x1b[1;34m' + "tharindu rukshan" + '\x1b[0m' )
 
 
