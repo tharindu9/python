e5cb1fb7fd92ac288cfe09dc9f461ed986dbfcfd
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2019-08-01 10:41:05 +0530

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,10 +1,22 @@
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 
 for i in range(1,9):
-    print("@", end=" ")
+    print("*", end=" ")
     if(i==3):
-        print("@","@")
+        print("*","*")
         for j in range(1,5):
-            print(" " , " ","@")
+            print(" " , " ","*")
 
 print()
 
@@ -12,26 +24,32 @@ for x in range(1,6):
 
     if(x==1):
         print()
-        print("  *","   *")
+        print(color.RED +"  *","   *" + color.END)
     if(x==2):
-        print("*   *","   *")
+        print(color.RED +"*   *","   *" +color.END)
     if(x==3):
-        print("*","  "*3 , "*")
+        print(color.RED  +"*","  "*3 , "*" +color.END)
     if(x==4):
-        print("  *","  ","*  ")
+        print(color.RED +"  *","  ","*  " +color.END)
     if(x==5):
-        print("   ","*" ,"  ")
+        print(color.RED +"   ","*" ,"  " +color.END)
+
+print()
 
 for k in range(1,6):
     if(k==1):
         for l in range(1,4):
             if(i!=3):
-                print("#"," "*3,"#")
+                print("*"," "*3,"*")
             else:
-                print("#",end="")
-
+                print("*",end="")
+    elif(k==2 or k==5):
+        print(" ",end=" ")
 
     else:
-        print("#" , end=" ")
+        print("*", end=" ")
+
+
+#sys.stdout.write('\x1b[1;34m' + "tharindu rukshan" + '\x1b[0m' )
 
 
